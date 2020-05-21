6b13fb7a4905eda8c3edbecd542440583b1ac16d
swetha udayakumar <65652722+swetha-learner@users.noreply.github.com>
2020-05-21 05:38:27 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader Loader=new FXMLLoader (Main.class.getResource ("sample.fxml"));
+        Parent root = Loader.load ();
+        primaryStage.setTitle("REGISTRATION FORM");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
